60fc137b4e830f44303e339c5f5e9aae121ef090
Gabriel Ferraz <sudoferraz@gmail.com>
2026-02-20 16:21:51 +0200

    chore: bump creem to v0.1.1

--- Formula/creem.rb
+++ Formula/creem.rb
@@ -5,7 +5,7 @@ class Creem < Formula
   desc "Command-line interface for Creem"
   homepage "https://creem.io"
   url "https://github.com/armitage-labs/creem-cli/releases/download/v0.1.1/creem-cli-0.1.1.tgz"
-  sha256 "f03f158abc2b6c654e5dbc9662fdb15aa17df46b302e86135d2ca5a562046a65"
+  sha256 "bf59f6d9157c63290c2084c3c2d9abc353b5e338be1fd0dcc8711533af16e8f0"
   license "MIT"
 
   depends_on "node@22"
